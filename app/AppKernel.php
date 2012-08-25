808772281df8f6bbac94c2e35a232ad00e4e60f6
Ph3nol <ph3@slynett.com>
2012-08-25 13:30:23 +0200

    Say hello to our FriendshipBundle! :)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,9 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+
+            // Coodit bundles
+            new Coo\FriendshipBundle\CooFriendshipBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
